2316c3d1ed0fff5d474dd627d588b329715aefa1
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2020-12-09 23:33:46 +0100

    testirala povezanost sa bazom na oba backenda i uradila citanje+pisanje xml dokumenta u exist bazu

---
+++ backend-organ-vlasti/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xmldb.api.base.XMLDBException;
+
+import com.example.demo.constants.Constants;
+import com.example.demo.database.ExistManager;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+	
+	@Autowired
+	private ExistManager existManager;
+	
+	@GetMapping(value = "/test1")
+	public void test1() throws ClassNotFoundException, InstantiationException, IllegalAccessException, XMLDBException {
+		File file = new File("data/test1.xml");
+		this.existManager.save(Constants.COLLECTIONS_PREFIX + "/test", "test1.xml", file);
+	}
+
+}
