8850140aeedca04600978ef803023701271c0df8
unknown <nikolicpetar91@yahoo.com>
2021-01-05 19:13:33 +0100

    izgenerisan servis za kreiranje zalbi kod organa vlasti i napravljen enpoint

---
+++ backend-organ-vlasti/src/main/java/com/example/demo/endpoint/EndpointConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.endpoint;
+
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.ws.zalba.CreateZalbaPortImpl;
+
+@Configuration
+public class EndpointConfig {
+
+	@Autowired
+	private Bus bus;
+
+	@Bean
+	public Endpoint helloMessageEndpoint() {
+		EndpointImpl endpoint = new EndpointImpl(bus, new CreateZalbaPortImpl());
+		endpoint.publish("/createZalba");
+		return endpoint;
+	}
+
+}
