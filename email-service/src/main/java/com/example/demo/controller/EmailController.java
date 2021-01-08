3482b202041286dd6ef76f29c22bf5447d37dc73
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-01-08 12:40:09 +0100

    sitne izmene kdo email servica

--- email-service/src/main/java/com/example/demo/controller/EmailController.java
+++ email-service/src/main/java/com/example/demo/controller/EmailController.java
@@ -1,9 +1,5 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,19 +8,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.xml.sax.SAXException;
 
 import com.example.demo.service.EmailService;
 
 @RestController
-@RequestMapping(value = "/email", consumes = MediaType.TEXT_XML_VALUE)
+@RequestMapping(value = "/email", consumes = MediaType.TEXT_XML_VALUE, produces = MediaType.TEXT_XML_VALUE)
 public class EmailController {
 	
 	@Autowired
 	private EmailService emailService;
 	
-	@PostMapping(produces = MediaType.TEXT_XML_VALUE)
-	public ResponseEntity<Void> sendEmail(@RequestBody String xml) throws ParserConfigurationException, SAXException, IOException {
+	@PostMapping
+	public ResponseEntity<Void> sendEmail(@RequestBody String xml) {
 		this.emailService.sendEmail(xml);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
--- email-service/src/main/java/com/example/demo/parser/DOMParser.java
+++ email-service/src/main/java/com/example/demo/parser/DOMParser.java
@@ -1,27 +1,22 @@
-package com.example.demo.service;
+package com.example.demo.parser;
 
 import static org.apache.xerces.jaxp.JAXPConstants.JAXP_SCHEMA_LANGUAGE;
 import static org.apache.xerces.jaxp.JAXPConstants.W3C_XML_SCHEMA;
 
-import java.io.IOException;
 import java.io.StringReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+
+import com.example.demo.exception.MyException;
 
 @Component
 public class DOMParser {
-	
-	@Autowired
-	private DOMErrorHandler errorHandler;
-	
+		
 	private DocumentBuilderFactory builderFactory;
 	
 	public DOMParser() {
@@ -34,10 +29,14 @@ public class DOMParser {
 		this.builderFactory.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
 	}
 	
-	public Document buildDocument(String xml) throws ParserConfigurationException, SAXException, IOException {
-		DocumentBuilder builder = this.builderFactory.newDocumentBuilder();
-		builder.setErrorHandler(this.errorHandler);
-		return builder.parse(new InputSource(new StringReader(xml)));
+	public Document buildDocument(String xml) {
+		try {
+			DocumentBuilder builder = this.builderFactory.newDocumentBuilder();
+			return builder.parse(new InputSource(new StringReader(xml)));
+		}
+		catch(Exception e) {
+			throw new MyException(e);
+		}
 	}
 	
 }
