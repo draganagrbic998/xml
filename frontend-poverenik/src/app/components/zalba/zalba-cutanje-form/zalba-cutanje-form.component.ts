66e8f9638b8258e98860dfaf084893fccd9650c7
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-01-10 13:01:31 -0800

    redila problem kod xonomy kod formi za zalbu

--- frontend-poverenik/src/app/components/zalba/zalba-cutanje-form/zalba-cutanje-form.component.ts
+++ frontend-poverenik/src/app/components/zalba/zalba-cutanje-form/zalba-cutanje-form.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SNACKBAR_CLOSE, SNACKBAR_ERROR, SNACKBAR_ERROR_OPTIONS, SNACKBAR_SUCCESS_OPTIONS } from 'src/app/constants/snackbar';
@@ -13,7 +13,7 @@ declare const Xonomy: any;
   templateUrl: './zalba-cutanje-form.component.html',
   styleUrls: ['./zalba-cutanje-form.component.sass']
 })
-export class ZalbaCutanjeFormComponent implements AfterViewInit {
+export class ZalbaCutanjeFormComponent implements AfterViewInit, AfterViewChecked {
 
   constructor(
     private zalbaService: ZalbaService,
@@ -52,6 +52,13 @@ export class ZalbaCutanjeFormComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void{
+    const detaljiXml = '<Detalji></Detalji>';
+    const detaljiEditor = document.getElementById('detaljiEditor');
+    const detaljiSpecifikacija = this.xonomyService.detaljiSpecifikacija;
+    Xonomy.render(detaljiXml, detaljiEditor, detaljiSpecifikacija);
+  }
+
+  ngAfterViewChecked(): void{
     const detaljiXml = '<Detalji></Detalji>';
     const detaljiEditor = document.getElementById('detaljiEditor');
     const detaljiSpecifikacija = this.xonomyService.detaljiSpecifikacija;
--- frontend-poverenik/src/app/interceptor/auth.interceptor.ts
+++ frontend-poverenik/src/app/interceptor/auth.interceptor.ts
@@ -19,7 +19,6 @@ export class AuthInterceptor implements HttpInterceptor {
     if (!this.authService.getUser()){
       return next.handle(request);
     }
-    console.log(this.authService.getUser());
     request = request.clone({
       setHeaders: {
         Authorization: this.authService.getUser().token
