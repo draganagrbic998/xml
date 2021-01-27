23e9f8b5edf691536a3a43c9648008f0d8986fed
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-01-27 21:03:00 +0100

    sitna izmenica :D

--- backend-poverenik/src/main/java/com/example/demo/repository/xml/OdgovorExist.java
+++ backend-poverenik/src/main/java/com/example/demo/repository/xml/OdgovorExist.java
@@ -39,7 +39,12 @@ public class OdgovorExist implements ExistInterface {
 	
 	@Override
 	public Document load(String documentId) {
-		return this.existManager.load(ODGOVOR_COLLECTION, documentId);
+		try {
+			return this.existManager.load(ODGOVOR_COLLECTION, documentId);
+		}
+		catch(Exception e) {
+			return null;
+		}
 	}
 
 	@Override
