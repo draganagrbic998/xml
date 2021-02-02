945a1872cd59565addbf312afa0ae8ce14c609a2
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-02-02 14:42:21 +0100

    sredila inicijalne podatke

--- backend-organ-vlasti/src/main/java/com/example/demo/common/DataInitializator.java
+++ backend-organ-vlasti/src/main/java/com/example/demo/common/DataInitializator.java
@@ -29,16 +29,16 @@ public class DataInitializator {
 	private static final String SLUZBENIK1 = Constants.INIT_FOLDER + "sluzbenik1.xml";
 	private static final String GRADJANIN1 = Constants.INIT_FOLDER + "gradjanin1.xml";
 
+	private static final String ZAHTEV_OBAVESTENJE1 = Constants.INIT_FOLDER + "zahtev_obavestenje1.xml";
 	private static final String ZAHTEV_UVID1 = Constants.INIT_FOLDER + "zahtev_uvid1.xml";
 	private static final String ZAHTEV_KOPIJA1 = Constants.INIT_FOLDER + "zahtev_kopija1.xml";
-	private static final String ZAHTEV_OBAVESTENJE1 = Constants.INIT_FOLDER + "zahtev_obavestenje1.xml";
 
 	private static final String OBAVESTENJE1 = Constants.INIT_FOLDER + "obavestenje1.xml";
 	private static final String ODBIJANJE1 = Constants.INIT_FOLDER + "odbijanje1.xml";
 
+	private static final String ZALBA_CUTANJE1 = Constants.INIT_FOLDER + "zalba_cutanje1.xml";
 	private static final String ZALBA_DELIMICNOST1 = Constants.INIT_FOLDER + "zalba_delimicnost1.xml";
 	private static final String ZALBA_ODLUKA1 = Constants.INIT_FOLDER + "zalba_odluka1.xml";
-	private static final String ZALBA_CUTANJE1 = Constants.INIT_FOLDER + "zalba_cutanje1.xml";
 
 	private static final String ODGOVOR1 = Constants.INIT_FOLDER + "odgovor1.xml";
 	private static final String ODGOVOR2 = Constants.INIT_FOLDER + "odgovor2.xml";
@@ -73,17 +73,16 @@ public class DataInitializator {
 		this.existManager.save(KorisnikExist.KORISNIK_COLLECTION, "sluzbenik@gmail.com", this.domParser.buildDocumentFromFile(SLUZBENIK1), KorisnikExist.KORISNIK_SCHEMA);
 		this.existManager.save(KorisnikExist.KORISNIK_COLLECTION, "draganaasd@gmail.com", this.domParser.buildDocumentFromFile(GRADJANIN1), KorisnikExist.KORISNIK_SCHEMA);
 
-		/*
-		this.existManager.save(ZahtevExist.ZAHTEV_COLLECTION, "1", this.domParser.buildDocumentFromFile(ZAHTEV_UVID1), ZahtevExist.ZAHTEV_SCHEMA);
-		this.existManager.save(ZahtevExist.ZAHTEV_COLLECTION, "2", this.domParser.buildDocumentFromFile(ZAHTEV_KOPIJA1), ZahtevExist.ZAHTEV_SCHEMA);
-		this.existManager.save(ZahtevExist.ZAHTEV_COLLECTION, "3", this.domParser.buildDocumentFromFile(ZAHTEV_OBAVESTENJE1), ZahtevExist.ZAHTEV_SCHEMA);
+		this.existManager.save(ZahtevExist.ZAHTEV_COLLECTION, "1", this.domParser.buildDocumentFromFile(ZAHTEV_OBAVESTENJE1), ZahtevExist.ZAHTEV_SCHEMA);
+		this.existManager.save(ZahtevExist.ZAHTEV_COLLECTION, "2", this.domParser.buildDocumentFromFile(ZAHTEV_UVID1), ZahtevExist.ZAHTEV_SCHEMA);
+		this.existManager.save(ZahtevExist.ZAHTEV_COLLECTION, "3", this.domParser.buildDocumentFromFile(ZAHTEV_KOPIJA1), ZahtevExist.ZAHTEV_SCHEMA);
 
 		this.existManager.save(OdlukaExist.ODLUKA_COLLECTION, "1", this.domParser.buildDocumentFromFile(OBAVESTENJE1), OdlukaExist.ODLUKA_SCHEMA);
 		this.existManager.save(OdlukaExist.ODLUKA_COLLECTION, "2", this.domParser.buildDocumentFromFile(ODBIJANJE1), OdlukaExist.ODLUKA_SCHEMA);
 
-		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "1", this.domParser.buildDocumentFromFile(ZALBA_DELIMICNOST1), ZalbaExist.ZALBA_SCHEMA);
-		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "2", this.domParser.buildDocumentFromFile(ZALBA_ODLUKA1), ZalbaExist.ZALBA_SCHEMA);
-		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "3", this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaExist.ZALBA_SCHEMA);
+		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "1", this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaExist.ZALBA_SCHEMA);
+		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "2", this.domParser.buildDocumentFromFile(ZALBA_DELIMICNOST1), ZalbaExist.ZALBA_SCHEMA);
+		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "3", this.domParser.buildDocumentFromFile(ZALBA_ODLUKA1), ZalbaExist.ZALBA_SCHEMA);
 
 		this.existManager.save(OdgovorExist.ODGOVOR_COLLECTION, "1", this.domParser.buildDocumentFromFile(ODGOVOR1), OdgovorExist.ODGOVOR_SCHEMA);
 		this.existManager.save(OdgovorExist.ODGOVOR_COLLECTION, "2", this.domParser.buildDocumentFromFile(ODGOVOR2), OdgovorExist.ODGOVOR_SCHEMA);
@@ -91,17 +90,17 @@ public class DataInitializator {
 	    this.existManager.save(ResenjeExist.RESENJE_COLLECTION, "1", this.domParser.buildDocumentFromFile(RESENJE1), ResenjeExist.RESENJE_SCHEMA);
 	    this.existManager.save(ResenjeExist.RESENJE_COLLECTION, "2", this.domParser.buildDocumentFromFile(RESENJE2), ResenjeExist.RESENJE_SCHEMA);
 	    this.existManager.save(ResenjeExist.RESENJE_COLLECTION, "3", this.domParser.buildDocumentFromFile(RESENJE3), ResenjeExist.RESENJE_SCHEMA);
-		
+
+		this.fusekiManager.add(ZahtevRDF.ZAHTEV_GRAPH, this.domParser.buildDocumentFromFile(ZAHTEV_OBAVESTENJE1), ZahtevRDF.ZAHTEV_SHAPE);
 		this.fusekiManager.add(ZahtevRDF.ZAHTEV_GRAPH, this.domParser.buildDocumentFromFile(ZAHTEV_UVID1), ZahtevRDF.ZAHTEV_SHAPE);
 		this.fusekiManager.add(ZahtevRDF.ZAHTEV_GRAPH, this.domParser.buildDocumentFromFile(ZAHTEV_KOPIJA1), ZahtevRDF.ZAHTEV_SHAPE);
-		this.fusekiManager.add(ZahtevRDF.ZAHTEV_GRAPH, this.domParser.buildDocumentFromFile(ZAHTEV_OBAVESTENJE1), ZahtevRDF.ZAHTEV_SHAPE);
 
 		this.fusekiManager.add(OdlukaRDF.ODLUKA_GRAPH, this.domParser.buildDocumentFromFile(OBAVESTENJE1), OdlukaRDF.ODLUKA_SHAPE);
 		this.fusekiManager.add(OdlukaRDF.ODLUKA_GRAPH, this.domParser.buildDocumentFromFile(ODBIJANJE1), OdlukaRDF.ODLUKA_SHAPE);
 
+		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaRDF.ZALBA_SHAPE);
 		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_DELIMICNOST1), ZalbaRDF.ZALBA_SHAPE);
 		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_ODLUKA1), ZalbaRDF.ZALBA_SHAPE);
-		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaRDF.ZALBA_SHAPE);
 		
 		this.fusekiManager.add(OdgovorRDF.ODGOVOR_GRAPH, this.domParser.buildDocumentFromFile(ODGOVOR1), OdgovorRDF.ODGOVOR_SHAPE);
 		this.fusekiManager.add(OdgovorRDF.ODGOVOR_GRAPH, this.domParser.buildDocumentFromFile(ODGOVOR2), OdgovorRDF.ODGOVOR_SHAPE);
@@ -109,7 +108,7 @@ public class DataInitializator {
 		this.fusekiManager.add(ResenjeRDF.RESENJE_GRAPH, this.domParser.buildDocumentFromFile(RESENJE1), ResenjeRDF.RESENJE_SHAPE);
 		this.fusekiManager.add(ResenjeRDF.RESENJE_GRAPH, this.domParser.buildDocumentFromFile(RESENJE2), ResenjeRDF.RESENJE_SHAPE);
 		this.fusekiManager.add(ResenjeRDF.RESENJE_GRAPH, this.domParser.buildDocumentFromFile(RESENJE3), ResenjeRDF.RESENJE_SHAPE);
-		*/
+
 	}
 
 }
--- backend-poverenik/src/main/java/com/example/demo/common/DataInitializator.java
+++ backend-poverenik/src/main/java/com/example/demo/common/DataInitializator.java
@@ -1,8 +1,5 @@
 package com.example.demo.common;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -26,9 +23,9 @@ public class DataInitializator {
 	private static final String POVERENIK1 = Constants.INIT_FOLDER + "poverenik1.xml";
 	private static final String GRADJANIN1 = Constants.INIT_FOLDER + "gradjanin1.xml";
 
+	private static final String ZALBA_CUTANJE1 = Constants.INIT_FOLDER + "zalba_cutanje1.xml";
 	private static final String ZALBA_DELIMICNOST1 = Constants.INIT_FOLDER + "zalba_delimicnost1.xml";
 	private static final String ZALBA_ODLUKA1 = Constants.INIT_FOLDER + "zalba_odluka1.xml";
-	private static final String ZALBA_CUTANJE1 = Constants.INIT_FOLDER + "zalba_cutanje1.xml";
 
 	private static final String ODGOVOR1 = Constants.INIT_FOLDER + "odgovor1.xml";
 	private static final String ODGOVOR2 = Constants.INIT_FOLDER + "odgovor2.xml";
@@ -47,7 +44,7 @@ public class DataInitializator {
 	private DOMParser domParser;
 
 	@EventListener(ContextRefreshedEvent.class)
-	public void dataInit() throws UnsupportedEncodingException, IOException {
+	public void dataInit() {
 
 		this.existManager.dropCollection(KorisnikExist.KORISNIK_COLLECTION);
 		this.existManager.dropCollection(ZalbaExist.ZALBA_COLLECTION);
@@ -59,10 +56,9 @@ public class DataInitializator {
 		this.existManager.save(KorisnikExist.KORISNIK_COLLECTION, "poverenik@gmail.com", this.domParser.buildDocumentFromFile(POVERENIK1), KorisnikExist.KORISNIK_SCHEMA);
 		this.existManager.save(KorisnikExist.KORISNIK_COLLECTION, "draganaasd@gmail.com", this.domParser.buildDocumentFromFile(GRADJANIN1), KorisnikExist.KORISNIK_SCHEMA);
 
-		/*
-		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "1", this.domParser.buildDocumentFromFile(ZALBA_DELIMICNOST1), ZalbaExist.ZALBA_SCHEMA);
-		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "2", this.domParser.buildDocumentFromFile(ZALBA_ODLUKA1), ZalbaExist.ZALBA_SCHEMA);
-		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "3", this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaExist.ZALBA_SCHEMA);
+		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "1", this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaExist.ZALBA_SCHEMA);
+		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "2", this.domParser.buildDocumentFromFile(ZALBA_DELIMICNOST1), ZalbaExist.ZALBA_SCHEMA);
+		this.existManager.save(ZalbaExist.ZALBA_COLLECTION, "3", this.domParser.buildDocumentFromFile(ZALBA_ODLUKA1), ZalbaExist.ZALBA_SCHEMA);
 
 		this.existManager.save(OdgovorExist.ODGOVOR_COLLECTION, "1", this.domParser.buildDocumentFromFile(ODGOVOR1), OdgovorExist.ODGOVOR_SCHEMA);
 		this.existManager.save(OdgovorExist.ODGOVOR_COLLECTION, "2", this.domParser.buildDocumentFromFile(ODGOVOR2), OdgovorExist.ODGOVOR_SCHEMA);
@@ -71,9 +67,9 @@ public class DataInitializator {
 	    this.existManager.save(ResenjeExist.RESENJE_COLLECTION, "2", this.domParser.buildDocumentFromFile(RESENJE2), ResenjeExist.RESENJE_SCHEMA);
 	    this.existManager.save(ResenjeExist.RESENJE_COLLECTION, "3", this.domParser.buildDocumentFromFile(RESENJE3), ResenjeExist.RESENJE_SCHEMA);
 
+		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaRDF.ZALBA_SHAPE);
 		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_DELIMICNOST1), ZalbaRDF.ZALBA_SHAPE);
 		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_ODLUKA1), ZalbaRDF.ZALBA_SHAPE);
-		this.fusekiManager.add(ZalbaRDF.ZALBA_GRAPH, this.domParser.buildDocumentFromFile(ZALBA_CUTANJE1), ZalbaRDF.ZALBA_SHAPE);
 		
 		this.fusekiManager.add(OdgovorRDF.ODGOVOR_GRAPH, this.domParser.buildDocumentFromFile(ODGOVOR1), OdgovorRDF.ODGOVOR_SHAPE);
 		this.fusekiManager.add(OdgovorRDF.ODGOVOR_GRAPH, this.domParser.buildDocumentFromFile(ODGOVOR2), OdgovorRDF.ODGOVOR_SHAPE);
@@ -81,7 +77,7 @@ public class DataInitializator {
 		this.fusekiManager.add(ResenjeRDF.RESENJE_GRAPH, this.domParser.buildDocumentFromFile(RESENJE1), ResenjeRDF.RESENJE_SHAPE);
 		this.fusekiManager.add(ResenjeRDF.RESENJE_GRAPH, this.domParser.buildDocumentFromFile(RESENJE2), ResenjeRDF.RESENJE_SHAPE);
 		this.fusekiManager.add(ResenjeRDF.RESENJE_GRAPH, this.domParser.buildDocumentFromFile(RESENJE3), ResenjeRDF.RESENJE_SHAPE);
-*/
+
 	}
 
 }
